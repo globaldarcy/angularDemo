769c939060a0f55e1ae462897b5b99314354a2ee
Shawn <289904316@qq.com>
2017-11-09 17:17:15 +0800

    test

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -9,6 +9,8 @@ export class NewsComponent implements OnInit {
   url = 'http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=1';
   jurl = 'http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=1&callback=JSONP_CALLBACK';
   list = [];
+  postUrl = 'http://localhost:3000/dologin';
+
   private headers = new Headers({'Content-Type': 'application/json'});
   constructor(private http: Http, private jsonp: Jsonp) {
   }
@@ -19,7 +21,7 @@ export class NewsComponent implements OnInit {
     let _that = this;
     this.http.get(this.url).subscribe(function (data) {
       // console.log(JSON.parse(data['_body']));
-      _that.list = JSON.parse(data._body).result;
+      _that.list = JSON.parse(data['_body']).result;
       console.log(_that.list);
     }, function (err) {
       console.log(err);
@@ -30,7 +32,7 @@ export class NewsComponent implements OnInit {
     let _that = this;
     this.jsonp.get(this.jurl).subscribe(function (data) {
       // console.log(JSON.parse(data['_body']));
-      _that.list = data._body.result;
+      _that.list = data['_body'].result;
       console.log(_that.list);
     }, function (err) {
       console.log(err);
@@ -38,6 +40,10 @@ export class NewsComponent implements OnInit {
   }
 
   postData() {
-
+    this.http.post(this.postUrl, JSON.stringify({'username': ''}), {headers: this.headers}).subscribe(function(data) {
+      console.log(data);
+    }, function(err) {
+      console.log(err);
+    });
   }
 }
