33e36867c707ee20c692bb4f36b35dc5aa9ac233
Shawn <289904316@qq.com>
2017-11-09 23:51:03 +0800

    test

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit, ViewEncapsulation, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class FooterComponent implements OnInit {
+  @Input() msg: string;
+  @Input() run: object;
+  @Input() search;
+  @Input() parent;
+  public childInfo = '子组件的数据';
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  childFn() {
+    this.parent(this.childInfo);
+  }
+
+}
